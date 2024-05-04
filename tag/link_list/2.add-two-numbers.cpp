c599e1262d335c9fc4be157d5313556d872488fc
Yejiajun227 <9063173+jiajun227@user.noreply.gitee.com>
2024-05-04 20:51:34 +0800

    Daily Question 5.4

---
+++ tag/link_list/2.add-two-numbers.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ * @lcpr version=30122
+ *
+ * [2] 两数相加
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *head = nullptr, *tail = nullptr;
+        int optional = 0;
+        while (l1 || l2) {
+            int num1 = l1 ? l1->val: 0;
+            int num2 = l2 ? l2->val: 0;
+            int sum = num1 + num2 + optional;
+            if (!head) {
+                head = tail = new ListNode(sum%10);
+            }else {
+                tail->next = new ListNode(sum%10);
+                tail = tail->next;
+            }
+            optional = sum / 10;
+            if (l1) {
+                l1 = l1->next;
+            }
+            if (l2) {
+                l2 = l2->next;
+            }
+        }
+        if (optional) {
+            tail->next = new ListNode(optional);
+        }
+        return head;
+
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [2,4,3]\n[5,6,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n[0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [9,9,9,9,9,9,9]\n[9,9,9,9]\n
+// @lcpr case=end
+
+ */
